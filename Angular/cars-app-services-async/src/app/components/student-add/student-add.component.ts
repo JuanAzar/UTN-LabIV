58c9aec432465db1f72acdc0e9e0c02070a11d56
Juan Azar <Juan.J.Azar@ey.com>
2020-10-23 17:05:50 -0300

    Changes in Students application

---
+++ Angular/cars-app-services-async/src/app/components/student-add/student-add.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Student } from 'src/app/models/student';
+import { StudentService } from 'src/app/services/student.service';
+
+@Component({
+  selector: 'app-student-add',
+  templateUrl: './student-add.component.html',
+  styleUrls: ['./student-add.component.css']
+})
+export class StudentAddComponent implements OnInit {
+  firstName : string;
+  lastName : string;
+  dni : string;
+  email : string;
+  address : string;
+  message : string;
+
+  constructor(private studentService: StudentService) { }
+
+  ngOnInit() {
+    
+  }
+
+  addStudent(){
+    let student = new Student();
+    student.firstName = this.firstName;
+    student.lastName = this.lastName;
+    student.dni = this.dni;
+    student.email = this.email;
+    student.address = this.address;
+
+    this.studentService.add(student)
+     
+    this.message = "Student successfully added";
+  }
+}
