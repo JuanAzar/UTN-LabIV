f734680e800344cc4e02fa247a5cbb5ef585972c
Juan Jose Azar <juan.azar@gmail.com>
2023-06-29 21:03:50 -0300

    Adding Routing Example app with Angular 16

---
+++ Angular/RoutingExample/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { StudentAddComponent } from './components/student-add/student-add.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentViewComponent } from './components/student-view/student-view.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent,
+    StudentAddComponent,
+    StudentListComponent,
+    StudentViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/RoutingExample/src/app/models/student.ts
@@ -0,0 +1,8 @@
+export class Student {
+    public studentId: number = 0;
+    public lastName: string = "";
+    public firstName: string = "";
+    public dni: string = "";
+    public email: string = "";
+    public address: string = "";
+}
